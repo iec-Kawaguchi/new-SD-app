bd5409af5d8a01c25e75bfecd0f74fda5816f5bd
kawaguchi <kawaguchi@iec.co.jp>
2025-12-01 11:44:33 +0900

    修正

--- js/mock-ui.js
+++ js/mock-ui.js
@@ -35,10 +35,16 @@
         </h2>
         <ul id="menu-items-1" class="cursor-pointer">
             <a href="media-plan-list.html" data-visible-for="iec, supplier, customer">
-            <li class="flex items-center ml-3 py-2 px-4 text-gray-700 border-l-2 border-gray-300 hover:border-gray-500 hover:font-bold">   
-                <span class="material-symbols-outlined text-[1.2rem] mr-1">dashboard</span>
-                募集一覧
-            </li>
+                <li class="flex items-center ml-3 py-2 px-4 text-gray-700 border-l-2 border-gray-300 hover:border-gray-500 hover:font-bold">   
+                    <span class="material-symbols-outlined text-[1.2rem] mr-1">dashboard</span>
+                    募集一覧
+                </li>
+            </a>
+            <a href="applications-download.html" data-visible-for="iec, supplier, customer">
+                <li class="flex items-center ml-3 py-2 px-4 text-gray-700 border-l-2 border-gray-300 hover:border-gray-500 hover:font-bold">   
+                    <span class="material-symbols-outlined text-[1.2rem] mr-1">download</span>
+                    申込DL
+                </li>
             </a>
            
         </ul>
